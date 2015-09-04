42265f9d95f5997ba10cf2361ef6e635ad6ebd21
ForeverL <609670166@qq.com>
2015-09-04 15:33:18 +0800

    Create Coin.java

---
+++ Coin.java
@@ -0,0 +1,31 @@
+public class Coin {
+	private static int[] coin={2,2,2,2,2,2,2,2,2,2};
+	private static int index=-1;
+	
+	public static void main(String[] args) {
+		int len=coin.length;
+		check(coin,0,len-1);
+		System.out.println(index);
+	}
+
+	private static void check(int[] arr,int start,int end) {
+		int len=end-start+1;
+		if(len==2){
+			if(arr[start]!=arr[end])
+				index=start;
+		}
+		
+		if(len==1){
+			if(arr[0]!=arr[start])
+				index=start;
+		}
+		if(len>2){
+			boolean temp=(arr[start]==arr[start+1]);
+			if(temp){
+				check(arr,start+2,end);
+			}else{
+				check(arr,start,start+1);
+			}
+		}
+	}
+}
